88801fdde61fe5319ca66a4e299318ace46c3928
Juan José <juanjosemagaa@hotmail.com>
2025-02-20 17:23:06 -0600

    Agregar archivos de proyecto.

---
+++ L01_2023-MM-658_2022-RL-652/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace L01_2023_MM_658_2022_RL_652
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
