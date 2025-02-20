49b428dfe57a435a02b62fac0ccc51e9d9e738fc
Juan José <juanjosemagaa@hotmail.com>
2025-02-20 17:54:11 -0600

    Modelos

--- L01_2023-MM-658_2022-RL-652/Models/Restaurante.cs
+++ L01_2023-MM-658_2022-RL-652/Models/Restaurante.cs
@@ -1,7 +1,37 @@
 ﻿using System.ComponentModel.DataAnnotations;
 namespace L01_2023_MM_658_2022_RL_652.Models
 {
-    public class Restaurante
+    public class clientes
     {
+        [Key]
+        public int clienteId { get; set; }
+        public string? nombreCliente { get; set; }
+        public string? direccion { get; set; }
+    }
+
+    public class pedidos
+    {
+        [Key]
+        public int pedidoId { get; set; }
+        public int motoristaId { get; set; }
+        public int clienteId { get; set; }
+        public int platoId { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+    }
+
+    public class platos
+    {
+        [Key]
+        public int platoId { get; set; }
+        public string? nombrePlato { get; set; }
+        public decimal precio { get; set; }
+    }
+
+    public class motoristas
+    {
+        [Key]
+        public int motoristaId { get; set; }
+        public string? nombreMotorista { get; set; }
     }
 }
---
+++ L01_2023-MM-658_2022-RL-652/Models/restauranteContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace L01_2023_MM_658_2022_RL_652.Models
+{
+    public class RestauranteContext : DbContext
+    {
+        public RestauranteContext(DbContextOptions<RestauranteContext> options) : base(options)
+        {
+
+        }
+        public DbSet<clientes> clientes { get; set; }
+        public DbSet<pedidos> pedidos { get; set; }
+        public DbSet<platos> platos { get; set; }
+        public DbSet<motoristas> motoristas { get; set; }
+
+    }
+}
