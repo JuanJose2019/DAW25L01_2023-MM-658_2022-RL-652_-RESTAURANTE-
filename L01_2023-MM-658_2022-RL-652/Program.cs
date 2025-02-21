cb0cebd39fe9055c7577a0a5de3f58fee46880f6
Juan José <juanjosemagaa@hotmail.com>
2025-02-20 18:06:54 -0600

    Conexion Actualizada

--- L01_2023-MM-658_2022-RL-652/Program.cs
+++ L01_2023-MM-658_2022-RL-652/Program.cs
@@ -1,3 +1,6 @@
+using L01_2023_MM_658_2022_RL_652.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Inyeccion por dependencia del string de conexion al contexto
+builder.Services.AddDbContext<RestauranteContext>(options =>
+    options.UseSqlServer(
+            builder.Configuration.GetConnectionString("restauranteDbConnection")
+        )
+);
 
 var app = builder.Build();
 
